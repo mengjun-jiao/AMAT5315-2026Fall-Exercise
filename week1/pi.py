6712dff243a1ddcc27fa32d24d79519a77d06573
mengjun-jiao <mengjun02113@gmail.com>
2026-09-02 19:09:33 +0800

    Implement Monte Carlo pi estimation

---
+++ week1/pi.py
@@ -0,0 +1,14 @@
+import random
+
+
+def estimate_pi(n, seed):
+    rng = random.Random(seed)
+    points_inside_circle = 0
+
+    for _ in range(n):
+        x = rng.random()
+        y = rng.random()
+        if x * x + y * y <= 1:
+            points_inside_circle += 1
+
+    return 4 * points_inside_circle / n
