e73f3b0081b865646da2446470248d4571f8d5e0
mengjun-jiao <mengjun02113@gmail.com>
2026-09-09 18:15:18 +0800

    Complete week 2 Part 1 Rust greeting exercise

---
+++ week2/md/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn greeting() -> &'static str {
+    "Hello, world!"
+}
+
+#[cfg(test)]
+mod tests {
+    use super::greeting;
+
+    #[test]
+    fn greeting_returns_hello_world() {
+        assert_eq!(greeting(), "Hello, world!");
+    }
+}
